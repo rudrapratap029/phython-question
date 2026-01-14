6ae7dfbe2f8ee4597ac4b8d74f24f71aa9a62871
rudrapratap029 <rudrapratap112005@gmail.com>
2026-01-14 11:11:38 +0530

    First commit

---
+++ index.py
@@ -0,0 +1,26 @@
+
+# Take input from user and print thr sum of two number ..............
+
+
+a= int(input("Enter first number : "))
+b= int(input("enter second number : "))
+sum = a+b
+print(sum)
+  
+
+# cost of 1 pen is 15rs & and cost of notebook is 40rs. if a children buy 1 pen &2 notebook find the total cost...........
+pen= 15
+note = 40 
+total_cost= (pen*1 + note*2 )
+print(total_cost)
+
+
+
+# a car travel 120km in 3 hrs find the average speed
+distance_travel = 120
+time_taken= 3
+average_speed = distance_travel/time_taken
+print(average_speed)
+
+
+
