32bbe95f9efe400938abeae3d3bd740e907e29ed
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-01-23 16:46:53 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,23 @@
+import math
+
+print(math.sqrt(625))
+
+print(math.factorial(6))
+
+print(math.floor(4.7))
+print(math.ceil(4.2))
+
+print(math.pow(5, 3))
+
+print(math.sin(math.radians(90)))
+print(math.cos(math.radians(90)))
+print(math.tan(math.radians(90)))
+
+
+print(math.gcd(36, 60))
+
+print(math.exp(3))
+
+print(math.log10(1000))
+
+print(math.radians(180))
