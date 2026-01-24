990e591a4a6e1640f1f2f3af80561b98c20ae854
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-01-24 15:19:38 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -8,17 +8,7 @@ sum = a+b
 print(sum)
   
 
-# cost of 1 pen is 15rs & and cost of notebook is 40rs. if a children buy 1 pen &2 notebook find the total cost...........
-pen= 15
-note = 40 
-total_cost= (pen*1 + note*2 )
-print(total_cost)
 
 
 
-# a car travel 120km in 3 hrs find the average speed..............................
-distance_travel = 120
-time_taken= 3
-average_speed = distance_travel/time_taken
-print(average_speed)
-print(average_speed)
+
