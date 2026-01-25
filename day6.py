ee5aa6418a9ab546cb1162538516bed7dc3c9b88
Rudra Pratap Singh <rudrapratap112005@gmail.com>
2026-01-25 21:22:37 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,23 @@
+# 1. Simple Function (No parameters)
+
+def say_hello():
+    print("Hello, world!")
+
+say_hello()
+
+
+# 2. Function with Parameters
+
+def greet(name):
+    print("Hello,", name)
+
+greet("Rudra")
+
+# 3. Function with Return Value
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print("Sum:", result)
+
